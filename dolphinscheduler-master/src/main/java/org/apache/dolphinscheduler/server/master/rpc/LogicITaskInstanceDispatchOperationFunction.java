245670a6182728e18a9cb8eefd7374db9cb70f5d
Hong Wang <xunhongwang@outlook.com>
2025-06-24 13:20:23 +0800

    Fix dependent task cannot kill completely

--- dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/rpc/LogicITaskInstanceDispatchOperationFunction.java
+++ dolphinscheduler-master/src/main/java/org/apache/dolphinscheduler/server/master/rpc/LogicITaskInstanceDispatchOperationFunction.java
@@ -24,6 +24,7 @@ import org.apache.dolphinscheduler.plugin.task.api.utils.LogUtils;
 import org.apache.dolphinscheduler.server.master.runner.execute.MasterTaskExecutionContextHolder;
 import org.apache.dolphinscheduler.server.master.runner.execute.MasterTaskExecutor;
 import org.apache.dolphinscheduler.server.master.runner.execute.MasterTaskExecutorFactoryBuilder;
+import org.apache.dolphinscheduler.server.master.runner.execute.MasterTaskExecutorHolder;
 import org.apache.dolphinscheduler.server.master.runner.execute.MasterTaskExecutorThreadPoolManager;
 
 import lombok.extern.slf4j.Slf4j;
@@ -64,8 +65,10 @@ public class LogicITaskInstanceDispatchOperationFunction
                     .createMasterTaskExecutor(taskExecutionContext);
             if (masterTaskExecutorThreadPool.submitMasterTaskExecutor(masterTaskExecutor)) {
                 log.info("Submit LogicTask: {} to MasterTaskExecutorThreadPool success", taskInstanceName);
+                MasterTaskExecutorHolder.putMasterTaskExecuteRunnable(masterTaskExecutor);
                 return LogicTaskDispatchResponse.success(taskInstanceId);
             } else {
+                MasterTaskExecutionContextHolder.removeTaskExecutionContext(taskInstanceId);
                 log.error("Submit LogicTask: {} to MasterTaskExecutorThreadPool failed", taskInstanceName);
                 return LogicTaskDispatchResponse.failed(taskInstanceId, "MasterTaskExecutorThreadPool is full");
             }
